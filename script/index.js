e45d43474b963df53135a09c0d9ca38b61546317
nehalpanchall <nehalpanchal40@gmail.com>
2024-12-18 10:40:55 -0500

    render items data into item container dynamically using Array of objects

---
+++ script/index.js
@@ -0,0 +1,16 @@
+let itemsContainer = document.querySelector(".items-container");
+
+items.forEach((i, index) => {
+  itemsContainer.innerHTML += `<div class="item-container">
+    <img class="item-image" src="${i.image}" alt="Image 1" />
+    <div class="ratings">${i.rating.stars} ⭐ | ${i.rating.count}k</div>
+    <div class="company-name">${i.company}</div>
+    <div class="item-name">${i.item_name}</div>
+    <div class="price">
+      <span class="current-price">Rs ${i.current_price}</span>
+      <span class="original-price">Rs ${i.original_price}</span>
+      <span class="discount">(${i.discount_percentage}% OFF)</span>
+    </div>
+    <button class="btn-add-bag">Add to Bag</button>
+  </div>`;
+});
