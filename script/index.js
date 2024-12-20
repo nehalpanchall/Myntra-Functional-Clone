4f455d82d465ae48fab342b08a015fb483987dc0
nehalpanchall <nehalpanchal40@gmail.com>
2024-12-20 01:43:40 -0500

    count and display number of bag items

--- script/index.js
+++ script/index.js
@@ -1,22 +1,28 @@
-const addToBag = (id) => {
-  alert(id);
-};
+let bagItem = [];
 
-(() => {
+function addToBag(itemId) {
+  bagItem.push(itemId);
+  let count = document.querySelector(".bag-item-count");
+  count.innerHTML = bagItem.length;
+}
+
+function displayItem() {
   let itemsContainer = document.querySelector(".items-container");
 
-  items.forEach((i) => {
+  items.forEach((item) => {
     itemsContainer.innerHTML += `<div class="item-container">
-          <img class="item-image" src="${i.image}" alt="Image 1" />
-          <div class="ratings">${i.rating.stars} ⭐ | ${i.rating.count}k</div>
-          <div class="company-name">${i.company}</div>
-          <div class="item-name">${i.item_name}</div>
+          <img class="item-image" src="${item.image}" alt="Image 1" />
+          <div class="ratings">${item.rating.stars} ⭐ | ${item.rating.count}k</div>
+          <div class="company-name">${item.company}</div>
+          <div class="item-name">${item.item_name}</div>
           <div class="price">
-            <span class="current-price">Rs ${i.current_price}</span>
-            <span class="original-price">Rs ${i.original_price}</span>
-            <span class="discount">(${i.discount_percentage}% OFF)</span>
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
           </div>
-          <button onclick="addToBag('${i.id}')" class="btn-add-bag">Add to Bag</button>
+          <button onclick="addToBag(${item.id})" class="btn-add-bag">Add to Bag</button>
         </div>`;
   });
-})();
+}
+
+displayItem();
