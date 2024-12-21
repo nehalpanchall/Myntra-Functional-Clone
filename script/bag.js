45061dd0b1b21d9967aeade9f2b5cfe6c22d0053
nehalpanchall <nehalpanchal40@gmail.com>
2024-12-21 14:10:42 -0500

    render and display bag items based on bag count

--- script/bag.js
+++ script/bag.js
@@ -1,30 +1,61 @@
+let bagItemObj;
+
 (() => {
+  bagItemsObj();
   displayBagItems();
+  //   console.log(bagItemObj);
 })();
+
+function bagItemsObj() {
+  bagItemObj = bagItems.map((itemId) => {
+    for (let i = 0; i < items.length; i++) {
+      if (itemId == items[i].id) {
+        return items[i];
+      }
+    }
+  });
+
+  // Use ForEach instead of foor loop (forEach does not return anything, it just modify)
+  //   bagItemObj = bagItems.map((itemId) => {
+  //     let obj = null;
+  //     items.forEach((element) => {
+  //       if (itemId == element.id) {
+  //         obj = element;
+  //       }
+  //     });
+  //     return obj;
+  //   });
+}
 
 function displayBagItems() {
   let bagItemsContainer = document.querySelector(".bag-items-container");
 
-  bagItemsContainer.innerHTML += `<div class="bag-item-container">
+  bagItemObj.forEach((item) => {
+    bagItemsContainer.innerHTML += generateItem(item);
+  });
+}
+
+function generateItem(items) {
+  return `<div class="bag-item-container">
             <div class="item-left-part">
-              <img class="bag-item-img" src="../images/4.jpg" />
+              <img class="bag-item-img" src="../${items.image}" />
             </div>
             <div class="item-right-part">
-              <div class="company">ADIDAS</div>
+              <div class="company">${items.company}</div>
               <div class="item-name">
-                Men Printed Polo Collar Indian Cricket ODI Jersey
+              ${items.tem_name}
               </div>
               <div class="price-container">
-                <span class="current-price">Rs 999</span>
-                <span class="original-price">Rs 999</span>
-                <span class="discount-percentage">(0% OFF)</span>
+                <span class="current-price">Rs ${items.current_price}</span>
+                <span class="original-price">Rs ${items.original_price}</span>
+                <span class="discount-percentage">(${items.discount_percentage}% OFF)</span>
               </div>
               <div class="return-period">
-                <span class="return-period-days">14 days</span> return available
+                <span class="return-period-days">${items.return_period} days</span> return available
               </div>
               <div class="delivery-details">
                 Delivery by
-                <span class="delivery-details-days">10 Oct 2023</span>
+                <span class="delivery-details-days">${items.delivery_date}</span>
               </div>
             </div>
 
